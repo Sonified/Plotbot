8d1c237c1d98c17450c7d4abbdd282a8f92b6721
Robert <robertalexandermusic@gmail.com>
2025-02-25 01:18:48 -0800

    streamlines the install process

--- Install_Scripts/2_setup_env.sh
+++ Install_Scripts/2_setup_env.sh
@@ -4,5 +4,5 @@ echo "🔹 Creating 'plotbot_env' environment..."
 conda env create -f environment.yml -y
 echo "✅ Environment created!"
 echo "" 
-echo "Copy and paste the following command, including the period, to run script #3: ./install_scripts/3_register_kernel.sh"
+echo "Copy and paste the following command, including the period, to  register Plotbot as a Jupyter kernel: ./install_scripts/3_register_kernel.sh"
 echo "" 
