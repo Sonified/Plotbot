5540084578622e1a7ad280e52d3199df055b162f
Robert <robertalexandermusic@gmail.com>
2025-02-24 23:50:52 -0800

    simplified install script

--- Install_Scripts/1_init_conda.sh
+++ Install_Scripts/1_init_conda.sh
@@ -4,11 +4,6 @@ echo "🔹 Initializing Conda..."
 conda init zsh 2>/dev/null || conda init bash
 echo "✅ Conda has been initialized (if it wasn't already)."
 
-# Disable auto-activation of base environment
-echo "🔹 Disabling auto-activation of (base) environment..."
-conda config --set auto_activate_base false
-echo "✅ Auto-activation of (base) is now OFF."
-
 # Reload terminal settings.  Source is enough.
 if [ -f ~/.zshrc ]; then
     echo "✅ Reloading terminal settings..."
@@ -18,7 +13,4 @@ elif [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
 
-echo "✅ Conda initialization complete." 
-echo "" 
-echo "Copy and paste the following command, including the period, to run script #2: ./install_scripts/2_setup_env.sh"
-echo "" 
+echo "✅ Conda initialization complete."
